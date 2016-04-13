4f7577f2bc38f401289fcc17a02fc6c2013a205f
Will Richards <awr@eduroam-160-10.dyn.carleton.edu>
2016-04-12 22:01:59 -0500

    All my changes are commited

--- temperature.c
+++ temperature.c
@@ -1,11 +1,17 @@
 #include <stdio.h> // required for printf
 
 int main() {
-   float fahr;
-   printf("What is the temperature in degrees Fahrenheit?\n");
-   scanf("%f", &fahr);
-   float cels = (fahr - 32) * 5/9;
-   printf("%.1f degrees Fahrenheit is %.3f degrees Celcius.\n", fahr,cels);
-
-
+    float fahr;
+    float celc;
+    printf("What is the temperature in degrees Fahrenheit?\n");
+    if (scanf("%f", &fahr) != 1) {                                  //returns error if input is not number
+        printf("Invalid input - please input a float\n");
+        return 0;
+    }
+    else {
+        celc = (fahr - 32) * 5/9;                            \
+        printf("%.1f degrees Fahrenheit is %.3f degrees celcius\n", fahr, celc);
+        
+    }
 }
+
